a136e8b019f465e94fbef68af8bb6f2c7aff89ab
Gabriel Pozo <jackgris2@gmail.com>
2024-03-13 19:06:03 -0300

    fix: running test suites
    Signed-off-by: Gabriel Pozo <jackgris2@gmail.com>

--- app/tests/controller/controller_suite_test.go
+++ app/tests/controller/controller_suite_test.go
@@ -1,6 +1,7 @@
 package controller_test
 
 import (
+	"battle-of-monsters/app/db"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -11,3 +12,12 @@ func TestController(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Controller Suite")
 }
+
+var _ = BeforeSuite(func() {
+	db.Connect()
+	db.Up()
+})
+
+var _ = AfterSuite(func() {
+	db.Down()
+})
--- app/tests/models/models_suite_test.go
+++ app/tests/models/models_suite_test.go
@@ -1,6 +1,7 @@
 package models_test
 
 import (
+	"battle-of-monsters/app/db"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -11,3 +12,12 @@ func TestModels(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Models Suite")
 }
+
+var _ = BeforeSuite(func() {
+	db.Connect()
+	db.Up()
+})
+
+var _ = AfterSuite(func() {
+	db.Down()
+})
